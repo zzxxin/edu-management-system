1c6d9552c3d24fbc3064216104e222f0af7c5595
id-zx <zhangxin11@leju.com>
2026-01-12 11:58:12 +0800

    tj

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 在 Heroku 上自动处理 HTTPS
+        // Heroku 的 *.herokuapp.com 域名会自动提供 SSL 证书
+        if (config('app.env') === 'production' && !empty($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
+            \URL::forceScheme('https');
+        }
     }
 }
